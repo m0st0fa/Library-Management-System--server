392d9d3368ff66f50c4f92318012f50eb539f0c9
mostofa kamal <mostofaoffical.net24@gmail.com>
2023-11-09 02:28:47 +0600

    return book issue fixed

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // this is Book Borrowed server database
 
     const BorrowedBookCollection = client.db('BorrowedBookDataBae').collection('BorrowedBook')
